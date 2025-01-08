41fd157f477b0d55264dbe75c2a1aa0ef7f67f34
joyeajin <dpwls31200@gmail.com>
2025-01-08 11:54:48 +0900

    ThemeProvider로 테마 설정하기

--- src/App.js
+++ src/App.js
@@ -2,44 +2,21 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const animation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    border-radius: 100px;
-  }
-  100%{
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${animation} 1s linear infinite;
-  display: flex;
-  align-items: center;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
-  span {
-    font-size: 36px;
-    &:hover {
-      font-size: 40px;
-    }
-  }
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <span>00</span>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
