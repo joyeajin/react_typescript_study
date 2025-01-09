666b2d74d9f6f7dc4809fbcfb49758ab93785cc3
joyeajin <dpwls31200@gmail.com>
2025-01-09 13:52:32 +0900

    interface로 프롭스 타입 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "styled-components";
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
