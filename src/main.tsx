17526a81018acc6c8f1f312aa434dfabc077e6b2
joyeajin <dpwls31200@gmail.com>
2025-01-09 15:06:31 +0900

    타입스크립트로 테마 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
